5ca39cd8df9077ce21f858d936e875c25b2e5245
Thapelo <thapelomoshoadiba@capitecbank.co.za>
2026-04-13 22:34:01 +0200

    initial-setup

--- src/main/java/com/capsule/corp/StatementServiceApplication.java
+++ src/main/java/com/capsule/corp/StatementServiceApplication.java
@@ -2,7 +2,9 @@ package com.capsule.corp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor")
 public class StatementServiceApplication {
